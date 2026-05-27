b3dd565269050f81ba955c3b0528fe6e85ece5c0
Pratyush <singhpratyush981@gmail.com>
2026-05-27 20:18:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// let res =  (a, b)=> {
+//     return a * b;
+// };
+
+// console.log(res(4, 5));
+
+// let res = (...nums) => {
+//     sum = 0;
+//     for (let i = 0; i < nums.length; i++){
+//         sum += nums[i];
+//     }
+//     return sum;
+// }
+
+// console.log(res(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// function vowels(str) {
+//     count = 0;
+//     v = 'aeiouAEIOU';
+//     for (let i = 0; i < str.length; i++){
+//         if (v.includes(str[i])) count++;
+//         else continue;
+//     }
+//     return count;
+// }
+
+// console.log(vowels('AaEeIiOoUu'))
+
+// function checkPallindrome(str) {
+//     rev = str.split('').reverse().join('');
+    
+//     return(rev===str);
+// }
+
+// checkPallindrome('mom');
+
+
+// setTimeout(() =>{
+//     console.log(4 * 2);
+// },3000);
+
+
+// function fn() {
+//     console.log('Hello');
+// }
+
+// function main(cb) {
+//     cb();
+//     cb();
+// }
+
+// main(fn);
+
+// let diff = 0
+// function sub(a, b) {
+//     diff += a - b;
+//     return diff;
+// }
+
+// console.log(sub(5, 2));
+// console.log(sub(5, 2));
+// console.log(sub(5, 2));
+// console.log(sub(5, 2));
+
+
+// let fact = (n) => {
+//     if (n == 0) return 1;
+//     return n * fact(n - 1);
+// }
+
+// console.log(fact(4));
+
+// (function IIFE() {
+//     console.log('Executed');
+// })()
+
+
